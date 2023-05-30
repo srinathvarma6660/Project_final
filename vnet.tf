4ccf2bf6ea104d56c7a369ff26bdc434f55ced7c
srinathvarma6660 <133969171+srinathvarma6660@users.noreply.github.com>
2023-05-30 17:28:03 +0530

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = "example-network"
   address_space       = ["11.0.0.0/16"]
-  location            =var.location
+  location            = var.location
   resource_group_name = var.resource_group_name
 }
 
