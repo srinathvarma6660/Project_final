7665cb49a50b292aaeb45e48cd4f09598a458263
srinathvarma6660 <133969171+srinathvarma6660@users.noreply.github.com>
2023-05-30 17:36:48 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "pocprojectstorageac"
-    container_name       = "poccontainer"
+    storage_account_name = "srinath1202"
+    container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    resource_group_name = "pocprojectrg"
+    resource_group_name = "practicepocproject"
   }
 }
